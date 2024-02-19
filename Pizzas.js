9d4dcfd5abd2f93f1addd1182300ff706524db0c
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:40:28 -0800

    Add files via upload

---
+++ Pizzas.js
@@ -0,0 +1,12 @@
+"use strict";
+//Written by :TAYYABA RAO
+// Date:18/feb/2024
+//Roll no:00388416
+//names of pizzas
+let pizzas = ["Peperoin", "Cuban", "BBQ chicken"];
+//loop
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza`);
+}
+//result
+console.log("I really love pizza!");
